c06a92f1123fc3152ef898622e85bc4df5ef73e8
disfordave <118544840+disfordave@users.noreply.github.com>
2024-11-07 23:05:13 +0900

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -31,21 +31,25 @@ function App() {
 
   return (
     <div className="bg-white dark:bg-gray-900 text-gray-950 dark:text-white min-h-screen">
-      <div className="max-w-2xl mx-auto p-4 ">
-          <div className={"flex justify-between items-center  mb-4"}>
-              <h1 className="text-2xl font-bold">Parliament</h1>
-              <select
-                  className="px-2 py-1 border-2 rounded-lg border-gray-200 dark:border-gray-700 appearance-none bg-white dark:bg-gray-900"
-                  value={theme} title={"Appearance"} onChange={
-                  (e) => setTheme(e.target.value as 'light' | 'dark' | 'auto')
-              }>
-                  <option value={'light'}>Light</option>
-                  <option value={'dark'}>Dark</option>
-                  <option value={'auto'}>Auto</option>
-              </select>
-          </div>
-          <Seats/>
-      </div>
+        <div className="max-w-2xl mx-auto p-4 ">
+            <div className={"flex justify-between items-center  mb-4"}>
+                <h1 className="text-2xl font-bold">Parliament</h1>
+                <select
+                    className="px-2 py-1 border-2 rounded-lg border-gray-200 dark:border-gray-700 appearance-none bg-white dark:bg-gray-900"
+                    value={theme} title={"Appearance"} onChange={
+                    (e) => setTheme(e.target.value as 'light' | 'dark' | 'auto')
+                }>
+                    <option value={'light'}>Light</option>
+                    <option value={'dark'}>Dark</option>
+                    <option value={'auto'}>Auto</option>
+                </select>
+
+            </div>
+            <Seats/>
+            <footer className={"p-8 text-center"}>
+                <p>&copy; {new Date().getFullYear()} parliament.pages.dev (beta)</p>
+            </footer>
+        </div>
     </div>
   );
 }
