1ac811bf59105164fea86c42b983a63197ff7d72
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-16 22:24:12 +0900

    feat: enhance ThemeButton component with motion effects and improved layout

--- src/components/ui/settings/ThemeButton.tsx
+++ src/components/ui/settings/ThemeButton.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useI18n } from "@/lib/zustandStore";
+import { motion } from "motion/react";
 
 type Theme = "light" | "dark" | "auto";
 
@@ -16,17 +17,14 @@ export default function ThemeButton() {
       const effectiveTheme =
         theme === "auto" ? (mediaQuery.matches ? "dark" : "light") : theme;
 
-      // update DOM
       document.documentElement.classList.toggle(
         "dark",
         effectiveTheme === "dark",
       );
     };
 
-    // Apply immediately
     applyTheme();
 
-    // Only watch system preference if we're in auto mode
     if (theme === "auto") {
       mediaQuery.addEventListener("change", applyTheme);
       return () => mediaQuery.removeEventListener("change", applyTheme);
@@ -41,16 +39,89 @@ export default function ThemeButton() {
     }
   }, [theme]);
 
+  const themes = [
+    {
+      value: "auto",
+      label: i("appearance.auto"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-4"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+    {
+      value: "light",
+      label: i("appearance.light"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-5"
+        >
+          <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+        </svg>
+      ),
+    },
+    {
+      value: "dark",
+      label: i("appearance.dark"),
+      icon: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="size-4"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+            clipRule="evenodd"
+          />
+        </svg>
+      ),
+    },
+  ];
+
   return (
-    <select
-      className="appearance-none rounded-lg border-2 border-gray-200 bg-white px-2 py-1 dark:border-gray-700 dark:bg-gray-900"
-      value={theme}
-      title={"Appearance"}
-      onChange={(e) => setTheme(e.target.value as "light" | "dark" | "auto")}
-    >
-      <option value={"light"}>{i("appearance.light")}</option>
-      <option value={"dark"}>{i("appearance.dark")}</option>
-      <option value={"auto"}>{i("appearance.auto")}</option>
-    </select>
+    <>
+      <div className="flex items-center justify-center gap-[0.125rem] rounded-full bg-gray-200 p-1 dark:bg-gray-700">
+        {themes.map((t) => (
+          <motion.div className="relative size-6" key={t.value}>
+            {t.value === theme ? (
+              <motion.div
+                layoutId="button"
+                transition={{
+                  duration: 0.3,
+                }}
+                className="absolute size-6 rounded-full bg-violet-600 dark:bg-violet-400"
+              ></motion.div>
+            ) : (
+              <></>
+            )}
+            <button
+              onClick={() => setTheme(t.value as Theme)}
+              title={t.label}
+              className={`absolute flex size-6 items-center justify-center rounded-full transition-colors duration-300 ${
+                t.value === theme
+                  ? "text-white dark:text-gray-950"
+                  : "dark:text-white"
+              }`}
+            >
+              {t.icon}
+            </button>
+          </motion.div>
+        ))}
+      </div>
+    </>
   );
 }
