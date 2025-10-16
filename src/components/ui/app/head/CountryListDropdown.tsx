a20d65d3eb0b278bd4cb3d8623ad3bb1e28aa5f4
Dave <118544840+disfordave@users.noreply.github.com>
2025-10-16 21:51:55 +0900

    Enhance CountryListDropdown design with Motion

--- src/components/ui/app/head/CountryListDropdown.tsx
+++ src/components/ui/app/head/CountryListDropdown.tsx
@@ -6,6 +6,8 @@ import {
 import { countries } from "@/data/countries";
 import { useEffect, useRef, useState } from "react";
 import { ChevronDownIcon } from "@/components/icons/Icons";
+import { Country } from "@/types";
+import { AnimatePresence, motion } from "motion/react";
 
 export default function CountryListDropdown() {
   const { selectedCountry, setSelectedCountry } = useSelectedCountry();
@@ -20,6 +22,20 @@ export default function CountryListDropdown() {
     }
   }, [openCountryList]);
 
+  function select(country: Country) {
+    setSelectedCountry(country);
+    const findCountry = countries.find((c) => c.name === country.name);
+    if (findCountry) {
+      setParties(findCountry.parties);
+      setSelectedParties(findCountry.parties);
+    } else {
+      setSelectedCountry(null);
+      setParties([]);
+      setSelectedParties([]);
+    }
+    setOpenCountryList(false);
+  }
+
   return (
     <div
       tabIndex={0}
@@ -33,9 +49,13 @@ export default function CountryListDropdown() {
         onClick={() => {
           setOpenCountryList(!openCountryList);
         }}
-        className="w-full rounded-lg border-2 border-gray-200 bg-white p-2 dark:border-gray-700 dark:bg-gray-900"
+        className={`w-full rounded-lg border-2 border-gray-200 p-2 transition-colors duration-300 dark:border-gray-700 ${
+          openCountryList
+            ? "bg-gray-200 dark:bg-gray-700"
+            : "bg-white dark:bg-gray-900"
+        }`}
       >
-        <div className="flex gap-1 justify-between items-center">
+        <div className="flex items-center justify-between gap-1">
           {selectedCountry ? (
             <div className="flex gap-1">
               <span>{selectedCountry.name}</span>
@@ -46,76 +66,77 @@ export default function CountryListDropdown() {
               <span>Custom</span>
             </>
           )}
-          <ChevronDownIcon className={
-            `transition-transform duration-300 ${openCountryList ? "rotate-180" : ""}`
-          } />
+          <ChevronDownIcon
+            className={`transition-transform duration-300 ${openCountryList ? "rotate-180" : ""}`}
+          />
         </div>
       </button>
       <div className="relative">
-        {openCountryList && (
-          <>
-            <div
-              className="absolute top-0 z-[100] max-h-[50vh] w-full overflow-auto rounded-lg border-2 border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900"
-              ref={dropdownRef}
-            >
-              <ul>
-                <li>
-                  <button
-                    onClick={() => {
-                      setSelectedCountry(null);
-                      setParties([]);
-                      setSelectedParties([]);
-                      setOpenCountryList(false);
-                      return;
-                    }}
-                    className={`w-full p-2 transition-colors duration-300 ${
-                      !selectedCountry
-                        ? "bg-gray-200 dark:bg-gray-700"
-                        : "bg-white dark:bg-gray-900"
-                    }`}
-                  >
-                    <div className="flex gap-1">
-                      <span>Custom</span>
-                    </div>
-                  </button>
-                </li>
-                {countries
-                  .sort((a, b) => a.name.localeCompare(b.name))
-                  .map((country) => (
-                    <li key={country.name} value={country.name}>
-                      <button
-                        onClick={() => {
-                          setSelectedCountry(country);
-                          const findCountry = countries.find(
-                            (c) => c.name === country.name,
-                          );
-                          if (findCountry) {
-                            setParties(findCountry.parties);
-                            setSelectedParties(findCountry.parties);
-                          } else {
-                            setSelectedCountry(null);
-                            setParties([]);
-                            setSelectedParties([]);
-                          }
-                          setOpenCountryList(false);
-                        }}
-                        className={`w-full p-2 transition-colors duration-300 ${
-                          selectedCountry === country
-                            ? "bg-gray-200 dark:bg-gray-700"
-                            : "bg-white dark:bg-gray-900"
-                        }`}
+        <AnimatePresence>
+          {openCountryList && (
+            <>
+              <motion.div
+                key="modal"
+                transition={{
+                  duration: 0.3,
+                }}
+                initial={{ opacity: 0, translateY: -2 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                exit={{ opacity: 0, translateY: -2 }}
+                className="absolute top-0 z-[100] max-h-[50vh] w-full overflow-auto rounded-lg border-2 border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-900"
+                ref={dropdownRef}
+              >
+                <ul>
+                  <li>
+                    <button
+                      onClick={() => {
+                        setSelectedCountry(null);
+                        setParties([]);
+                        setSelectedParties([]);
+                        setOpenCountryList(false);
+                        return;
+                      }}
+                      className={`w-full p-2 transition-colors duration-300 ${
+                        !selectedCountry
+                          ? "bg-gray-200 dark:bg-gray-700"
+                          : "bg-white hover:bg-gray-100 dark:bg-gray-900 hover:dark:bg-gray-800"
+                      }`}
+                    >
+                      <div className="flex gap-1">
+                        <span>Custom</span>
+                      </div>
+                    </button>
+                  </li>
+                  {countries
+                    .sort((a, b) => a.name.localeCompare(b.name))
+                    .map((country) => (
+                      <li
+                        key={country.name}
+                        value={country.name}
+                        id={country.name}
                       >
-                        <div className="flex gap-1">
-                          <span>{country.name}</span>
-                          <span>{country.emoji}</span>
-                        </div>
-                      </button>
-                    </li>
-                  ))}
-              </ul>
-            </div>
-          </>
-        )}
+                        <button
+                          onClick={() => {
+                            select(country);
+                          }}
+                          className={`w-full p-2 transition-colors duration-300 ${
+                            selectedCountry === country
+                              ? "bg-gray-200 dark:bg-gray-700"
+                              : "bg-white hover:bg-gray-100 dark:bg-gray-900 hover:dark:bg-gray-800"
+                          }`}
+                        >
+                          <div className="flex gap-1">
+                            <span>{country.name}</span>
+                            <span>{country.emoji}</span>
+                          </div>
+                        </button>
+                      </li>
+                    ))}
+                </ul>
+              </motion.div>
+            </>
+          )}
+        </AnimatePresence>
       </div>
     </div>
   );
